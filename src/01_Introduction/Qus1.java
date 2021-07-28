acc25b5a6bc83812dd41f3c44169e83d2833a00f
Vijay Gawade <VijayGawade96@outlook.com>
2021-07-28 22:39:27 +0530

    Add assignment solution

---
+++ src/01_Introduction/Qus1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Qus1{
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I know your Name: ");
+        System.out.println("Welcome "+scanner.nextLine()+" !");
+    }   
+}
